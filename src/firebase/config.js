c3ecbf117218abba8fc0d6438b8af7a79869601e
dariodefreyne-ui <dario.de.freyne@gmail.com>
2026-06-05 01:10:57 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,6 +3,7 @@ console.log('Firebase config check:', {
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ? 'aanwezig' : 'ONTBREEKT',
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ? 'aanwezig' : 'ONTBREEKT',
 });
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
